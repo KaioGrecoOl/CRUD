7453760a55301615bfbc055217ab151ba6236010
Kaio G <kaiogreco91@gmail.com>
2022-11-07 21:51:48 -0300

    feat: cria conexão com o DB, e elabora query para salvar produtos e integração com axios

--- server/index.js
+++ server/index.js
@@ -1,5 +1,29 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+require('dotenv/config');
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB
+})
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+  const { name } = req.body;
+  const { price } = req.body;
+  const { category } = req.body;
+
+  let mysql = "INSERT INTO games ( name, price, category) VALUES (?, ?, ?)";
+  db.query(mysql, [name, price, category], (err, result) => {
+      console.log(err);
+    })
+})
 
 app.listen(3003, () => {
   console.log('Escutando porta 3003');
